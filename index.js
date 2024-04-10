21d9f823242c441c723895cd8bde1c7cdfd8f861
Virmerson Bento dos Santos <virmerson.dev@gmail.com>
2024-04-10 12:47:47 +0100

    Reading XLSX

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const http = require("http");
 const fs = require("fs");
-const evasionCalc = require("./evasion-service.js");
-const saidaJson = require("./saida.json")
 
-const express = require("express");
-const multer = require("multer");
+//read files
 const csvParser = require("csv-parser");
+const xlsx = require("xlsx");
 
-const Router = express.Router;
+//upload files
+const multer = require("multer");
+const upload = multer({ dest: "tmp/files/" });
 
-const upload = multer({ dest: "tmp/csv/" });
+//express - server
+const express = require("express");
 const app = express();
+const Router = express.Router;
 const router = new Router();
+
+
 const server = http.createServer(app);
 const port = 9000;
+
+//services
+const evasionCalc = require("./evasion-service.js");
+
+//data
+const saidaJson = require("./saida.json")
+
 
 router.post("/ingressos", upload.single("file"), function (req, res) {
   if (!req.file) {
@@ -54,31 +65,23 @@ router.post("/egressos", upload.single("file"), function (req, res) {
 });
 
 // ajuste router.post("/matriculas" para receber o arquivo em XLSX
-const multerXlsx = require("multer-xlsx");
-const uploadXlsx = multerXlsx({ dest: "tmp/xlsx/" });
 
-router.post("/matriculas-xlsx", uploadXlsx.single("file"), function (req, res) {
+
+router.post("/matriculas-xlsx", upload.single("file"), function (req, res) {
   if (!req.file) {
     return res.status(400).send("No file uploaded.");
   }
-  const fileRows = [];
-  const readableStream = fs.createReadStream(req.file.path)
-        .pipe(csvParser());
-  readableStream
-    .on("data", function (data) {
-      fileRows.push(data); // push each row
-    })
-    .on("end", function () {
-      console.log(fileRows); //contains array of arrays. Each inner array represents row of the csv file, with each element of it a column
-      fs.unlinkSync(req.file.path); // remove temp file
-      res.send("Finished reading xlsx file")
-      //process "fileRows" and respond
-    });
+  const workbook = xlsx.readFile(req  .file.path);
+  const sheet_name_list = workbook.SheetNames;
+  const xlData = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+  console.log(xlData);
+  res.send("Finished reading xlsx file")
 });
 
+
+
 // ajuste router.post("/matriculas" para receber o arquivo em CSV
-const multerCsv = require("multer-csv");
-const uploadCsv = multerCsv({ dest: "tmp/csv/" });
+
 
 router.post("/matriculas", upload.single("file"), function (req, res) {
   if (!req.file) {
@@ -104,7 +107,7 @@ router.get("/evasion-calc", function(req, res){
   res.json(saidaJson)
 })
 
-app.use("/upload-csv", router);
+app.use("/upload", router);
 
 function startServer() {
   server.listen(port, function () {
@@ -113,3 +116,6 @@ function startServer() {
 }
 
 setImmediate(startServer);
+
+
+//solve the problem: Error: Cannot find module 'multer-xlsx'  by running the command: npm install multer-xlsx
