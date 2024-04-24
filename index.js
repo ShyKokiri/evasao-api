487ab2806ccb024076cf930f532c87e33503fb62
Gabriel <gcosta147@gmail.com>
2024-04-24 08:01:47 -0400

    separando resposabilidades
    construindo um conversor de planilha para objetos que representam os ingressos

--- index.js
+++ index.js
@@ -30,7 +30,24 @@ router.post("/ingressos", upload.single("file"), function (req, res) {
   if (!req.file) {
     return res.status(400).send("No file uploaded.");
   }
+  let listIngressos = converterIngressosParaObjetos (req.file)
+  
+  // console.log(listaIngressos)
+
+  // let gt = new GeradorDeTaxa()
+  // let resultadoEvasao = gt.gerarEvasao(listIngressos, listaX, listaY, listaZ)
+  // res.json(resultadoEvasao)
+
+
+  //  fs.unlinkSync(req.file.path); // remove temp file
+  //res.send("Finished reading csv file")
+
+});
+
+
+ function converterIngressosParaObjetos(file){
   const fileRows = [];
+  const egressos=[];
   const readableStream = fs.createReadStream(req.file.path)
         .pipe(csvParser());
   readableStream
@@ -38,12 +55,22 @@ router.post("/ingressos", upload.single("file"), function (req, res) {
       fileRows.push(data); // push each row
     })
     .on("end", function () {
-      console.log(fileRows); //contains array of arrays. Each inner array represents row of the csv file, with each element of it a column
-      fs.unlinkSync(req.file.path); // remove temp file
-      res.send("Finished reading csv file")
-      //process "fileRows" and respond
+
+      let arrayEngressos = []
+      fileRows.forEach( (row)=>{
+
+        //convert cada row em OBjeto Dados do Egresso 
+        let egresso =  converteRowParaEgresso(row)
+        console.log(row)
+
+        //Adiciona no array de Egressos
+        egressos.push(egresso)
+      })
+     
     });
-});
+    return egressos
+ 
+  }
 
 router.post("/egressos", upload.single("file"), function (req, res) {
   if (!req.file) {
@@ -75,7 +102,7 @@ router.post("/matriculas-xlsx", upload.single("file"), function (req, res) {
   const sheet_name_list = workbook.SheetNames;
   let xlData = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]] );
 
-  let keys = ["ano", "faculdade", "curso", "codigo", "total_excluidos"]
+  let keys = ["ano", "faculdade", "curso", "excluidos", "total_excluidos"]
 
   xlData = xlData.map(row => {
     const newRow = {};
