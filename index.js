68617df4770fae25e0dbeb2d60c9ab3cb67bd97b
@virmerson <virmerson@htcursos.com>
2024-05-28 15:50:24 -0400

    ajustes cors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require("http");
-
+const cors = require("cors");
 
 const fs = require("fs");
 //read files
@@ -12,6 +12,8 @@ const upload = multer({ dest: "tmp/files/" });
 //express - server
 const express = require("express");
 const app = express();
+app.use(cors);
+
 const Router = express.Router;
 const router = new Router();
 
@@ -22,82 +24,85 @@ const port = 9000;
 const GeradorDeTaxa = require("./src/service/GeradorDeTaxa.js");
 
 //convertores
-const {converterIngressosParaObjetos,converterEgressosParaObjetos,converterMatriculasParaObjetos,converterExcluidosParaObjetos} = require("./src/converters/conversores.js");
+const {
+  converterIngressosParaObjetos,
+  converterEgressosParaObjetos,
+  converterMatriculasParaObjetos,
+  converterExcluidosParaObjetos,
+} = require("./src/converters/conversores.js");
 
 //data
-const saidaJson = require("./saida.json")
+const saidaJson = require("./saida.json");
 
 //Listas
 
-let listEgressos =[]
-let listIngressos =[]
+let listEgressos = [];
+let listIngressos = [];
 
 router.post("/ingressos", upload.single("file"), async function (req, res) {
   if (!req.file) {
     return res.status(400).send("No file uploaded.");
   }
-   listIngressos = await  converterIngressosParaObjetos (req.file)
-  
-  res.json(listIngressos)
+  listIngressos = await converterIngressosParaObjetos(req.file);
 
+  res.json(listIngressos);
 });
-
-
 
 router.post("/egressos", upload.single("file"), async function (req, res) {
   if (!req.file) {
     return res.status(400).send("No file uploaded.");
   }
-   listEgressos = await converterEgressosParaObjetos (req.file)
-  
-    res.json(listEgressos)
-  
+  listEgressos = await converterEgressosParaObjetos(req.file);
+
+  res.json(listEgressos);
 });
 
-
-router.get("/calc" ,  (req, res)=>{
+router.get("/calc", (req, res) => {
   // realizar o calc da evasao
-  if ( listEgressos.length==0  || listIngressos.length==0){
-    res.send("É necessário fazer o upload das listas de egressos e ingressos")
-  }else {
-
-    let gerenciadorDeTaxa = new GeradorDeTaxa()
-    let resultadoEvasao = gerenciadorDeTaxa.gerarTaxaEvasao(listIngressos, listEgressos)
-      res.json(resultadoEvasao)
+  if (listEgressos.length == 0 || listIngressos.length == 0) {
+    res.send("É necessário fazer o upload das listas de egressos e ingressos");
+  } else {
+    let gerenciadorDeTaxa = new GeradorDeTaxa();
+    let resultadoEvasao = gerenciadorDeTaxa.gerarTaxaEvasao(
+      listIngressos,
+      listEgressos
+    );
+    res.json(resultadoEvasao);
   }
-})
+});
 
 // ajuste router.post("/matriculas" para receber o arquivo em XLSX
 
+router.post(
+  "/matriculas-xlsx",
+  upload.single("file"),
+  async function (req, res) {
+    if (!req.file) {
+      return res.status(400).send("No file uploaded.");
+    }
+    listMatriculas = await converterMatriculasParaObjetos(req.file);
 
-router.post("/matriculas-xlsx", upload.single("file"), async function (req, res) {
-  if (!req.file) {
-    return res.status(400).send("No file uploaded.");
+    res.json(listMatriculas);
+
+    //how to set the key of the object
   }
-  listMatriculas = await converterMatriculasParaObjetos (req.file)
-  
-    res.json(listMatriculas)
-  
-  //how to set the key of the object
-});
+);
 
 router.post("/excluidos", upload.single("file"), async function (req, res) {
   if (!req.file) {
     return res.status(400).send("No file uploaded.");
   }
-  listExcluidos = await converterExcluidosParaObjetos (req.file)
-  
-    res.json(listExcluidos)
-  
+  listExcluidos = await converterExcluidosParaObjetos(req.file);
+
+  res.json(listExcluidos);
+
   //how to set the key of the object
 });
 
-
-
-router.get("/evasion-calc", function(req, res){
-  evasionCalc()
-  res.json(saidaJson)
-})
+router.get("/evasion-calc", function (req, res) {
+  evasionCalc();
+  res.json(saidaJson);
+});
 
 app.use("/upload", router);
 
@@ -108,4 +113,3 @@ function startServer() {
 }
 
 setImmediate(startServer);
-
