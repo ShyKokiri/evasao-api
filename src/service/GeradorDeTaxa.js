848c31573c35a6824088e73da856a860f6f86bf8
Gabriel <gcosta147@gmail.com>
2024-07-03 14:12:10 -0400

    03/07

--- src/service/GeradorDeTaxa.js
+++ src/service/GeradorDeTaxa.js
@@ -1,14 +1,18 @@
-module.exports=  class GeradorDeTaxa{
-    constructor(){
-       
-    }
-    gerarTaxaEvasao(listIngressos, listEgressos){
-        console.log("Gerando evasao ....")
-        return {
-            taxa:50,
-            listIngressos,
-            listEgressos
-        }
-    }  
-
-}
+module.exports = class GeradorDeTaxa {
+  constructor() {}
+  gerarTaxaEvasao(listIngressos, listEgressos) {
+    console.log("Gerando evasao ....");
+    return [
+      {
+        ano: 2010,
+        taxaSucesso: 50,
+        taxaEvasao: 50,
+      },
+      {
+        ano: 2010,
+        taxaSucesso: 50,
+        taxaEvasao: 50,
+      },
+    ];
+  }
+};
